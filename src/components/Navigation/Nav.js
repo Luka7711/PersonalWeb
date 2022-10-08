66b06a7400f9bf41d28987ed31f6d2cc9b10d23d
luk7711 <ulish6737@gmail.com>
2022-10-08 14:48:34 -0500

    added navigation bar mobile first

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,11 +1,24 @@
-function NavLinks({links}) {
-    let lis = links.map(link => <li>{link}</li>)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import styles from './Nav.module.css';
+import uuid from 'react-uuid';
+import { useState } from 'react';
+
+function NavLinks({links, displayMenu}) {
+    const slideMenu = displayMenu ? {right: "0"} : {right: "-100%"} 
+
+    let list = links.map(link => <li key={uuid()}>{link}</li>)
+    console.log(slideMenu)
     return (
-        <ul>{lis}</ul>
+        <ul className={styles.containerLinks} style={slideMenu}>
+            {list}
+        </ul>
     )
 }
 
 export default function Nav() {
+    const [displayMenu, setDisplayMenu] = useState(false);
+
     const links = [
         "About", 
         "Experience", 
@@ -14,9 +27,16 @@ export default function Nav() {
         "Contact"
     ]
 
+    function handleDisplayMenu() {
+        setDisplayMenu(!displayMenu);
+    }
+
     return (
-        <nav>
-            <NavLinks links={links}/>
+        <nav className={styles.nav}>
+            <span onClick={ () => handleDisplayMenu() } className={styles.iconBar}>
+                <FontAwesomeIcon icon={faBars} />
+            </span>
+            <NavLinks links={links} displayMenu={displayMenu}/>
         </nav>
     )
 }
